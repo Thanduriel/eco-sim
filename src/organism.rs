8e2e3c8454629a91e161d269de7a7899975c1b73
Robert Jendersie <rojendersie@alice.de>
2026-01-01 15:22:58 +0100

    improved grass generation

--- src/organism.rs
+++ src/organism.rs
@@ -1,10 +1,10 @@
+use crate::domain;
+use crate::{Surface, Terrain};
 use bevy::prelude::*;
 use bevy_prng::WyRand;
 use bevy_rand::prelude::*;
 use rand::prelude::*;
 use std::f32::consts::PI;
-use crate::{Terrain, Surface};
-use crate::domain;
 
 #[derive(Component, Default)]
 pub struct Organism {
@@ -33,7 +33,7 @@ const SPAWN_PROP: f32 = 0.01;
 const MIN_PROPAGATION_AGE: f32 = 2.0;
 
 pub fn propagate_organisms(
-//    time: Res<Time>,
+    //    time: Res<Time>,
     mut commands: Commands,
     organism_query: Query<(&Transform, &Organism)>,
     terrain_query: Query<&Terrain>,
@@ -51,8 +51,9 @@ pub fn propagate_organisms(
         if rng.random::<f32>() >= SPAWN_PROP {
             continue;
         }
-        
-        let area = Circle::new(1.0);
+
+        const SPAWN_RADIUS: f32 = 1.0;
+        let area = Circle::new(SPAWN_RADIUS);
         let p = area.sample_interior(&mut rng) + transform.translation.xz();
         if !domain::BOUNDS.contains(p) {
             continue;
@@ -61,18 +62,23 @@ pub fn propagate_organisms(
             continue;
         }
 
+        const ORIENTATION_MAX_RADIUS: f32 = 0.1;
+        let axis_circle = Circle::new(ORIENTATION_MAX_RADIUS);
+        let tip = axis_circle.sample_interior(&mut rng);
+        let axis = Vec3::new(tip.x, 1.0, tip.y).normalize();
+
+        const BELOW_SURFACE_DEPTH: f32 = 0.08;
         commands.spawn((
             Mesh3d(grass_assets.mesh.clone()),
             bevy::light::NotShadowCaster::default(),
             MeshMaterial3d(grass_assets.material.clone()),
-            Transform::from_translation(
-                Vec3::new(p.x, terrain.height_map.get_nearest(p) - 0.1, p.y),
-            )
+            Transform::from_translation(Vec3::new(
+                p.x,
+                terrain.height_map.get_nearest(p) - BELOW_SURFACE_DEPTH,
+                p.y,
+            ))
             .with_scale(Vec3::ZERO)
-            .with_rotation(Quat::from_axis_angle(
-                Vec3::new(0.0, 1.0, 0.0),
-                rng.random::<f32>() * 2.0 * PI,
-            )),
+            .with_rotation(Quat::from_axis_angle(axis, rng.random::<f32>() * 2.0 * PI)),
             Organism::default(),
         ));
         surface.veg_density.add_kernel(p, 0.125, 1.0);
