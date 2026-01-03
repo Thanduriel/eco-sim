7e9c1a5f94c796848128007c7f327aed9f03c98a
Robert Jendersie <rojendersie@alice.de>
2026-01-03 15:10:08 +0100

    dying grass releases surface space refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use bevy::prelude::*;
 use bevy::render::render_resource::Face;
 use bevy_prng::WyRand;
 use bevy_rand::prelude::*;
-use rand::prelude::*;
 use std::f32::consts::PI;
 
 use crate::camera_controller::*;
@@ -12,6 +11,7 @@ use crate::grass::{GrassAssets, create_grass_material, create_grass_mesh};
 use crate::terrain::*;
 
 mod camera_controller;
+mod player_inputs;
 mod color_map;
 mod domain;
 mod grass;
@@ -26,14 +26,14 @@ fn main() {
         })
         .insert_resource(grass::GrassAssets::default())
         .insert_resource(Time::<Fixed>::from_hz(60.0))
-        .insert_resource(FieldVisState::default())
+        .insert_resource(player_inputs::FieldVisState::default())
         .add_plugins(DefaultPlugins)
         .add_plugins(CameraControllerPlugin)
         .add_plugins(EntropyPlugin::<WyRand>::default())
         //      .add_plugins(ScreenSpaceAmbientOcclusionPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, input_system)
-        .add_systems(Update, vis_fields_system)
+        .add_systems(Update, player_inputs::input_system)
+        .add_systems(Update, player_inputs::vis_fields_system)
         .add_systems(FixedUpdate, organism::update_organisms)
         .add_systems(FixedUpdate, organism::propagate_organisms)
         .run();
@@ -46,7 +46,7 @@ fn setup_world(world: &mut World){
     });
 }*/
 
-/// set up a simple 3D scene
+/// set scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -125,105 +125,4 @@ fn setup(
             constant_object_thickness: 4.0,
         },
     ));
-}
-
-#[derive(Default, PartialEq)]
-enum FieldType {
-    #[default]
-    None,
-    Reset,
-    VegDensity,
-}
-
-#[derive(Resource, Default)]
-struct FieldVisState {
-    field_type: FieldType,
-}
-
-fn vis_fields_system(
-    mut meshes: ResMut<Assets<Mesh>>,
-    terrain_query: Query<(&Mesh3d, &Surface), With<Terrain>>,
-    key_input: Res<ButtonInput<KeyCode>>,
-    mut field_vis_state: ResMut<FieldVisState>,
-) {
-    // switch debug coloring
-    if key_input.pressed(KeyCode::F1) {
-        field_vis_state.field_type = FieldType::Reset;
-    }
-    if key_input.pressed(KeyCode::F2) {
-        field_vis_state.field_type = FieldType::VegDensity;
-    }
-
-    if field_vis_state.field_type == FieldType::None {
-        return;
-    }
-
-    let (mesh3d, surface) = terrain_query.single().unwrap();
-    if let Some(mut mesh) = meshes.get_mut(&mesh3d.0) {
-        match field_vis_state.field_type {
-            FieldType::None => panic!(),
-            FieldType::Reset => reset_terrain_color(&mut mesh),
-            FieldType::VegDensity => set_terrain_color(&mut mesh, &surface.veg_density),
-        };
-    }
-}
-
-fn input_system(
-    mut commands: Commands,
-    grass_assets: Res<GrassAssets>,
-    mut ray_cast: MeshRayCast,
-    terrain_query: Query<(), With<Terrain>>,
-    camera_query: Query<(&Camera, &GlobalTransform)>,
-    window_query: Query<&Window>,
-    mouse_button_input: Res<ButtonInput<MouseButton>>,
-    mut rng: Single<&mut WyRand, With<GlobalRng>>,
-) {
-    if !mouse_button_input.just_released(MouseButton::Right) {
-        return;
-    }
-
-    let Ok(window) = window_query.single() else {
-        return;
-    };
-
-    let Some(cursor_position) = window.cursor_position() else {
-        return;
-    };
-
-    let Ok((camera, camera_global_transform)) = camera_query.single() else {
-        return;
-    };
-
-    let Ok(ray) = camera.viewport_to_world(camera_global_transform, cursor_position) else {
-        return;
-    };
-
-    let filter = |entity| terrain_query.contains(entity);
-    let early_exit_test = |_entity| true;
-
-    // Ignore the visibility of entities. This allows ray casting hidden entities.
-    let visibility = RayCastVisibility::Any;
-
-    let settings = MeshRayCastSettings::default()
-        .with_filter(&filter)
-        .with_early_exit_test(&early_exit_test)
-        .with_visibility(visibility);
-
-    // Cast the ray with the settings, returning a list of intersections.
-    let hits = ray_cast.cast_ray(ray, &settings);
-
-    for (_, hit) in hits {
-        commands.spawn((
-            Mesh3d(grass_assets.mesh.clone()),
-            bevy::light::NotShadowCaster::default(),
-            MeshMaterial3d(grass_assets.material.clone()),
-            Transform::from_translation(hit.point - vec3(0.0, 0.1, 0.0))
-                .with_scale(Vec3::ZERO)
-                .with_rotation(Quat::from_axis_angle(
-                    Vec3::new(0.0, 1.0, 0.0),
-                    rng.random::<f32>() * 2.0 * PI,
-                )),
-            organism::Organism::default(),
-        ));
-    }
 }
